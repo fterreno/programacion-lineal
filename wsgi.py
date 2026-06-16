4b74fa6606c194552eec846bc68a154490868224
Florencia Sofia Terreno Monla <terrenoflorencia13@gmail.com>
2026-06-16 19:29:07 -0300

    feat: pythonanywhere

---
+++ wsgi.py
@@ -0,0 +1,9 @@
+import sys
+import os
+
+project_home = os.path.dirname(os.path.abspath(__file__))
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+from boundary import create_app
+application = create_app()
